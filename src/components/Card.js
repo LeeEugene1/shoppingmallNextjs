3bb344bae29dc12ffd8d5b247b2cad49b0d2dd50
LeeEugene1 <uwm1004@gmail.com>
2022-05-21 22:29:21 +0900

    how to bring image

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,10 @@ function Card({ item }) {
   // shoesList.map((item) => console.log(item));
   return (
     <div className="col-md-4">
+      <div
+        className="product-image"
+        style={{ backgroundImage:`url(https://codingapple1.github.io/shop/shoes${item.id + 1}.jpg)`}}
+      ></div>
       <h5>{item.title}</h5>
       {/* <p>{data[0].content}</p> */}
       <p>{item.price}</p>
